4f6cae0a8c5e3646907ea2aecb73591eeccea9de
evilkid <yassine_hell@hotmail.com>
2017-03-12 01:00:10 +0100

    Implementing edit method.

--- routes/form.js
+++ routes/form.js
@@ -4,23 +4,45 @@ var mongoose = require('mongoose');
 var Comment = mongoose.model('comments');
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    Comment.find(function (err, comments) {
-        console.log(comments);
-        res.render('form',
-            { title: 'FORM Page', comments: comments });
 
-    })
+    if (req.query.id) {
+        console.log("parasm", req.query.id);
+        Comment.find(function (err, comments) {
+            console.log(comments);
+
+            //or just iterate or the collection...
+            Comment.findById(req.query.id, function (err, comment) {
+                res.render('form',
+                    {title: 'FORM Page', comments: comments, comment: comment});
+            });
+        });
+    } else {
+        Comment.find(function (err, comments) {
+            console.log(comments);
+            res.render('form',
+                {title: 'FORM Page', comments: comments});
+        });
+    }
 });
 
 router.post('/', function (req, res, next) {
     console.log(req.body.comment);
 
-    new Comment({ title: req.body.comment })
-        .save(function (err, comment) {
+    if (req.body.id) {
+        Comment.findByIdAndUpdate(req.body.id, {$set: {title: req.body.comment}}, function (err, comment) {
             res.redirect('form');
         })
 
+    } else {
+        new Comment({title: req.body.comment})
+            .save(function (err, comment) {
+                res.redirect('form');
+            })
+    }
+
+
 });
+
 
 
 router.get('/delete/:id', function (req, res, next) {
